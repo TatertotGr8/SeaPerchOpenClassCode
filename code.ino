8f16df078b2f3cfbba1040e2856fc96bf692a1f0
Tate <thetatertotgamer54@gmail.com>
2023-03-16 13:50:11 -0600

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,31 @@
+     digitalWrite(IN1_PIN, HIGH);
+     digitalWrite(IN2_PIN, LOW);
+
+    //Front Right Y
+     digitalWrite(IN3_PIN, HIGH);
+    digitalWrite(IN4_PIN, LOW);
+
+     //Front Left Y 
+    digitalWrite(IN1_B_PIN, HIGH);
+    digitalWrite(IN2_B_PIN, LOW);
+
+    //Back Left Y
+    digitalWrite(IN3_B_PIN, HIGH);
+    digitalWrite(IN4_B_PIN, LOW);
+ 
+    //unkkown 
+    digitalWrite(IN1_C_PIN, HIGH);
+    digitalWrite(IN2_C_PIN, LOW);
+
+    //right foward 
+    digitalWrite(IN3_C_PIN, HIGH);
+    digitalWrite(IN4_C_PIN, LOW);
+    
+    //unknown 
+    digitalWrite(IN1_D_PIN, HIGH);
+    digitalWrite(IN2_D_PIN, LOW);
+
+    //Left Foward  
+    digitalWrite(IN3_D_PIN, HIGH);
+    digitalWrite(IN4_D_PIN, LOW); 
+      
